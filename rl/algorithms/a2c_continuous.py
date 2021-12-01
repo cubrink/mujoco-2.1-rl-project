3b386d5ea07585074261a9fe383733172a4013d5
Curtis Brinker <me@cubrink.com>
2021-12-01 08:20:12 -0600

    Changes for a2c antv3 test

--- rl/algorithms/a2c_continuous.py
+++ rl/algorithms/a2c_continuous.py
@@ -1,30 +1,25 @@
 import gym
+import time
 import torch
 import torch.nn as nn
 import torch.distributions as distributions
+
 from tqdm import tqdm
+from rl.utils.misc import as_numpy, as_tensor, get_device, init_mlp
 
 
 class Actor(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size):
+    def __init__(self, network_sizes, device=None):
         super().__init__()
-        log_std = -0.05 * torch.ones(output_size, dtype=torch.float32)
+        self.device = get_device(device=device)
+        log_std = -0.05 * torch.ones(network_sizes[-1], dtype=torch.float32)
         self.log_std = torch.nn.Parameter(torch.as_tensor(log_std))
-        self.mu_net = self.initialize_policy_net(input_size, hidden_size, output_size)
+        self.mu_net = init_mlp(network_sizes)
         self.buffers = {"logp_a_history": [], "episode_rewards": []}
-
-    def initialize_policy_net(self, input_size, hidden_size, output_size):
-        """
-        Initializes a network that, given an observation, outputs the mean values for 
-        the normal distribution that the action is sampled from
-        """
-        return nn.Sequential(
-            nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, output_size),
-        )
+        self.to(self.device)
 
     def forward(self, observation):
+        observation = as_tensor(observation, device=self.device)
         policy = distributions.Normal(
             loc=self.mu_net(observation), scale=torch.exp(self.log_std)
         )
@@ -48,19 +43,15 @@ class Actor(nn.Module):
 
 
 class Critic(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, network_sizes, device=None):
         super().__init__()
-        self.value_approximator = self.initialize_value_approximator(
-            input_size, hidden_size
-        )
+        self.device = get_device(device=device)
+        self.value_approximator = init_mlp(network_sizes)
         self.buffers = {"value_history": []}
-
-    def initialize_value_approximator(self, input_size, hidden_size):
-        return nn.Sequential(
-            nn.Linear(input_size, hidden_size), nn.ReLU(), nn.Linear(hidden_size, 1)
-        )
+        self.to(self.device)
 
     def forward(self, observation):
+        observation = as_tensor(observation, device=self.device)
         value = self.value_approximator(observation)
         self.buffers["value_history"].append(value)
         return value
@@ -74,21 +65,39 @@ class Critic(nn.Module):
 
 class ActorCritic(nn.Module):
     def __init__(
-        self, observation_space, action_space, hidden_size=64, lr=3e-4, gamma=0.99
+        self,
+        observation_space,
+        action_space,
+        model_dir,
+        hidden_sizes=(256, 256),
+        lr=3e-4,
+        gamma=0.99,
+        device=None,
     ):
         super().__init__()
-        super(ActorCritic, self).add_module(
-            "actor",
-            Actor(observation_space.shape[0], action_space.shape[0], hidden_size),
-        )
-        super(ActorCritic, self).add_module(
-            "critic", Critic(observation_space.shape[0], hidden_size)
-        )
+        self.device = get_device(device=device)
+        self.model_dir = model_dir
+        self.model_dir.mkdir(exist_ok=True, parents=True)
         self.gamma = gamma
+
+        actor_sizes = (observation_space.shape[0], *hidden_sizes, action_space.shape[0])
+        super(ActorCritic, self).add_module(
+            "actor", Actor(network_sizes=actor_sizes, device=device),
+        )
+        critic_sizes = (
+            observation_space.shape[0],
+            *hidden_sizes,
+            1,
+        )
+        super(ActorCritic, self).add_module(
+            "critic", Critic(network_sizes=critic_sizes, device=device)
+        )
+
         self.optim = torch.optim.Adam(self.parameters(), lr=lr)
+        self.to(self.device)
 
     def forward(self, observation):
-        observation = torch.Tensor(observation).float()
+        observation = as_tensor(observation, device=self.device)
         value = self.critic(observation)
         policy = self.actor(observation)
         return value, policy
@@ -105,9 +114,9 @@ class ActorCritic(nn.Module):
             rewards.insert(0, reward_to_go)
 
         # Convert values to tensors
-        q_t = torch.Tensor(rewards)
-        value_history = torch.Tensor(self.critic["value_history"])
-        logp_a_history = torch.stack(self.actor["logp_a_history"])
+        q_t = as_tensor(rewards, device=self.device)
+        value_history = torch.Tensor(self.critic["value_history"]).to(self.device)
+        logp_a_history = torch.stack(self.actor["logp_a_history"]).to(self.device)
 
         # Calcualte loss of each module
         advantage = q_t - value_history  # Q(s, a) - V(s)
@@ -126,6 +135,77 @@ class ActorCritic(nn.Module):
         # Reset buffers for actor/critic
         self.clear_buffers()
         return actor_loss, critic_loss
+
+    def train(self, env, steps):
+        state = env.reset()
+        stats = {"reward_history": [], "pi_loss": [], "v_loss": []}
+        max_reward = -100
+        episode_num = 0
+
+        for step in tqdm(range(steps)):
+            value, policy = self(state)
+            action, logp_a = self.actor.get_action(policy)
+
+            state, reward, done, _ = env.step(as_numpy(action))
+            self.actor["episode_rewards"].append(reward)
+
+            if done:
+                # Perform update at the end of each episode
+                pi_loss, v_loss = self.update()
+                stats["pi_loss"].append(pi_loss.item())
+                stats["v_loss"].append(v_loss.item())
+
+                # Test policy
+                render = episode_num % 50 == 0
+                test_result = self.test(env, render=render)
+                if test_result > max_reward:
+                    max_reward = test_result
+                    model_path = (
+                        self.model_dir / f"a2c-antv3-{int(test_result)}-{step}.pt"
+                    )
+                    torch.save(self.state_dict(), model_path)
+                state = env.reset()
+                episode_num += 1
+        return stats
+
+    def test(
+        self, env, max_steps=25_000, test_iterations=10, render=False, sleep_time=None
+    ):
+        episode_rewards = []
+
+        mse_threshold = 1e-4
+        stationary_count_threshold = 50
+
+        for episode in range(test_iterations):
+            stationary_count = 0
+            total_reward = 0
+            state_curr = env.reset()
+            for step in range(max_steps):
+                _, policy = self(state_curr)
+                action, _ = self.actor.get_action(policy)
+                state_next, reward, done, _ = env.step(as_numpy(action))
+
+                mse = ((state_curr - state_next) ** 2).sum()
+                if mse < mse_threshold:
+                    stationary_count += 1
+                if stationary_count >= stationary_count_threshold:
+                    done = True
+
+                state_curr = state_next
+                total_reward += reward
+
+                if render:
+                    env.render()
+                    if sleep_time:
+                        time.sleep(sleep_time)
+                if done:
+                    break
+            episode_rewards.append(total_reward)
+            render = False
+
+        mean_reward = sum(episode_rewards) / len(episode_rewards)
+        self.clear_buffers()
+        return mean_reward
 
     def clear_buffers(self):
         self.actor.clear_buffers()
@@ -168,7 +248,7 @@ if __name__ == "__main__":
             a2c.actor["episode_rewards"].append(reward)
 
             if done:
-                a2c.update()
+                pi_loss, v_loss = a2c.update()
                 break
 
         # if episode % 200 == 0:
